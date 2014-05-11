971ae9ebc9d62305bc05b578cad41de3986e4ab7
ssdosso <ssdosso@naver.com>
2014-05-11 15:32:57 +0900

    db

--- server/lib/database/drivers/Mysql.js
+++ server/lib/database/drivers/Mysql.js
@@ -10,46 +10,61 @@ var EventEmitter = process.EventEmitter
 
 
 
-var Mysqldb = DB_driver.extend({
+ module.exports  = DB_driver.extend({
     conn :null,
     connection : function() {
         var self = this;
 
-        function handleDisconnect() {
+        return  (function() {
+            if (this.get('pool') === true) {
+                self.conn = Mysql.createPool({
+                    host     : this.get('hostname'),
+                    user     : this.get('username'),
+                    password : this.get('password'),
+                    database : this.get('database')
+                });
 
-            self.conn = Mysql.createPool({
-                host     : this.get('hostname'),
-                user     : this.get('username'),
-                password : this.get('password'),
-                database : this.get('database')
-            });
-            this.conn.getConnection(function(err,connection) {
-                if(err)  {
-                    Baram.getInstance().log.warn(err);
-                }
+                self.conn.on('end', function(err) {
+                    console.log('mysql close');
+                });
+            } else {
 
-            });
+               this.conn = Mysql.createConnection({
+                   host     : this.get('hostname'),
+                   user     : this.get('username'),
+                   password : this.get('password'),
+                   database : this.get('database')
+                });
 
-//            self.conn.connect(function(err){
-//
-//                if(err) {
-//
-//                    Mono.getInstance().log.info('error when connecting to db:', err);
-//                    setTimeout(handleDisconnect.call(self), 2000);
-//                }
-//                Mono.getInstance().log.info('mysql connect success');
-//
-//            });
+                this.conn.connect(function(err) {
 
-            self.conn.on('end', function(err) {
-                console.log('mysql close');
-            });
+                    if (err) {
+                        switch(err.code) {
+                            case 'ER_BAD_DB_ERROR':
+                                console.log('ER_BAD_DB_ERROR: Unknown database')
+                                break;
+                            case 'ER_ACCESS_DENIED_ERROR':
+                                console.log(' ER_ACCESS_DENIED_ERROR: Access denied for user ')
+                                break;
+                            default :
+                                console.error('error connecting: ' + err.stack);
 
-        }
-
+                                break;
+                        }
+                    }
 
 
-        handleDisconnect.call(this);
+
+
+
+                    console.log('connected as id ' + self.conn.threadId);
+                })
+
+            }
+
+
+        }).call(this);
+
 
     },
     query : function(queryString, callback) {
@@ -178,4 +193,3 @@ var Mysqldb = DB_driver.extend({
 //
 //});
 
-exports = module.exports = Mysqldb;
